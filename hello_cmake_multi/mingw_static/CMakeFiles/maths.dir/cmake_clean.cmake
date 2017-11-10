663c345cc2e4f142c3e4c9dc32ad84fb1b17fcdc
Chatsrirung Anantaya <eeyac5@ad.nottingham.ac.uk>
2017-11-10 11:07:47 +0000

    test

---
+++ hello_cmake_multi/mingw_static/CMakeFiles/maths.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maths.dir/adder.cpp.obj"
+  "libmaths.pdb"
+  "libmaths.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maths.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
